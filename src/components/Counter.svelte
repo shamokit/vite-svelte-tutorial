b4dc7f826f86598038af2228680880aad776f0e4
shamokit <contact@shamokit.com>
2022-05-05 16:18:12 +0900

    移動

---
+++ src/components/Counter.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	let count: number = 1;
+	const increment = () => {
+		count += 1;
+	};
+</script>
+
+<button on:click={increment}>
+	Clicks: {count} {count === 1 ? 'time' : 'times'}
+</button>
+
+<style>
+	button {
+		font-family: inherit;
+		font-size: inherit;
+		padding: 1em 2em;
+		color: rgb(var(--main-color));
+		background-color: rgba(var(--main-color),.1);
+		border: 2px solid rgba(var(--main-color),1);
+		border-radius: 2em;
+		outline: none;
+		width: 200px;
+		font-variant-numeric: tabular-nums;
+		cursor: pointer;
+	}
+
+	button:hover,
+	button:active,
+	button:focus-visible {
+		background-color: rgb(var(--main-color));
+		border-color: rgba(var(--main-color),1);
+		color: #fff;
+	}
+</style>
