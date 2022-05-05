98b1815957e6c98be2d6de5be8067c0e67da4a1c
shamokit <contact@shamokit.com>
2022-05-05 16:18:24 +0900

    色をCSS変数に

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import Logo from "./components/Logo.svelte";
-	import Counter from "./lib/Counter.svelte";
+	import Counter from "./components/Counter.svelte";
 	import HelloWorld from "./components/HelloWorld.svelte";
 	import Paragraph from "./components/Paragraph.svelte";
 	import HtmlTest from "./components/HtmlTest.svelte";
@@ -20,6 +20,7 @@
 	:root {
 		font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
 			Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+		--main-color: 255, 62 ,0;
 	}
 
 	main {
@@ -29,7 +30,7 @@
 	}
 
 	h1 {
-		color: #ff3e00;
+		color: rgb(var(--main-color));
 		text-transform: uppercase;
 		font-size: 4rem;
 		font-weight: 100;
