d8f594701353dc38831cd6e1f42cc8879b6e7904
shamokit <contact@shamokit.com>
2022-05-05 15:35:56 +0900

    tsエラーが出ていたのでとりあえず解消

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import App from './App.svelte'
-
+const documentApp = document.getElementById('app')
 const app = new App({
-  target: document.getElementById('app')
+  target: documentApp ? documentApp : document.body
 })
 
 export default app
