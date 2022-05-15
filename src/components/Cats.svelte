040b12428458fc4a0cc1250ae0008027fd75caac
shamokit <contact@shamokit.com>
2022-05-15 23:42:05 +0900

    ts化

--- src/components/Cats.svelte
+++ src/components/Cats.svelte
@@ -1,17 +1,26 @@
-<script>
-	let cats = [
+<script lang="ts">
+	type Cat = {
+		id: string
+		name: string
+	}
+	let cats: Cat[] = [
 		{ id: 'J---aiyznGQ', name: 'Keyboard Cat' },
-		{ id: 'z_AbfPXTKms', name: 'Maru' },
+		{ id: 'z_AbfPXTKms' },
 		{ id: 'OUtn3pvWmpg', name: 'Henri The Existential Cat' }
 	];
+	const name = 'test'
 </script>
 
 <h1>The Famous Cats of YouTube</h1>
-
+{name}
 <ul>
 	{#each cats as {id, name}, index}
 		<li>{index}:<a target="_blank" href="https://www.youtube.com/watch?v={id}">
-			{name}
+			{#if name}
+				{name}
+			{:else}
+				{name}(ここではscript内でのnameではなく分割代入したほうが読まれる)
+			{/if}
 		</a></li>
 	{/each}
 </ul>
